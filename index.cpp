e3dca2a0c6a739f64844c240e536dd644e1488e7
Andrew <svt.andrey1631@gmail.com>
2020-02-26 21:15:30 +0200

    Добавили функцию
    Урра, получаеться))))

--- index.cpp
+++ index.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main()
 {
     std::cout << "Hello World!\n";
 	cout << "Can you see me?";
+
+}
+
+void func (int a)
+{
+  cout << a;
 }
