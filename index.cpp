9507fb147ec1f392c041468fc4bc711db2c83407
Andrew <svt.andrey1631@gmail.com>
2020-02-26 21:19:23 +0200

    Revert "Добавили функцию"
    This reverts commit e3dca2a0c6a739f64844c240e536dd644e1488e7.

--- index.cpp
+++ index.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 int main()
 {
     std::cout << "Hello World!\n";
 	cout << "Can you see me?";
-
-}
-
-void func (int a)
-{
-  cout << a;
 }
